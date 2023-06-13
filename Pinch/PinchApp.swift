b9306568cc923287268c8bd795a9520c2908c7a2
ChanYoung <a_b_ab@naver.com>
2023-06-13 09:16:16 +0900

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by 이찬영 on 2023/06/13.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
